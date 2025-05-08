305de86ad1561c11c82a2a445f24be08116c74c1
arthuryu <arthur422tp@gmail.com>
2025-05-08 15:13:57 +0800

    fixed format

--- src/utils/query_retrieval.py
+++ src/utils/query_retrieval.py
@@ -1,11 +1,13 @@
 """
 一般faiss內積檢索函式
 """
+
 import faiss
 import src.utils.word_embedding as word_embedding
 
+
 class Retrieval:
-    def __init__(self, text, model, embedding_dim): 
+    def __init__(self, text, model, embedding_dim):
         self.text = text
         self.model = None
         self.embedding_dim = embedding_dim
@@ -19,9 +21,8 @@ class Retrieval:
         index = faiss.IndexFlatIP(dimension)
         index.add(embeddings)
         return index
-    
 
-    def retrieve(self, query, text, index, k:int):
+    def retrieve(self, query, text, index, k: int):
         """
         檢索top-k
         """
@@ -30,7 +31,6 @@ class Retrieval:
         query_embedding = model.encode(query)
         query_embedding = query_embedding.reshape(1, -1)
         faiss.normalize_L2(query_embedding)
-        _, I = index.search(query_embedding, k)
+        _, I = index.search(query_embedding, k)  # noqa: E741
         retrieved_docs = [text[i] for i in I[0]]
         return retrieved_docs
-        
